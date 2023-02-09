fa0b082f1dce492cc07b1e1415ea197b7120f86c
Giljinsu <therin234@gmail.com>
2023-02-09 11:21:26 +0900

    add springsecurity

--- src/main/java/com/study/toy_springboots/controller/HomeController.java
+++ src/main/java/com/study/toy_springboots/controller/HomeController.java
@@ -1,5 +1,7 @@
 package com.study.toy_springboots.controller;
 
+import org.springframework.security.core.context.SecurityContextHolder;
+import org.springframework.security.core.userdetails.UserDetails;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 
@@ -7,6 +9,20 @@ import org.springframework.web.bind.annotation.RequestMapping;
 public class HomeController {
     @RequestMapping(value = {"","/","/main"})
     public String home() {
+        Object principal = SecurityContextHolder.getContext().getAuthentication().getPrincipal();
+        // 여기서 principal은 userdetails다
+        if (principal instanceof UserDetails) {
+            //로그인이 되어있을때 UserDetails 
+            System.out.println(((UserDetails)principal).getUsername());
+            System.out.println(((UserDetails)principal).getAuthorities());
+            System.out.println(((UserDetails)principal).getPassword());
+            System.out.println(((UserDetails)principal).isAccountNonExpired());
+            System.out.println(((UserDetails)principal).isAccountNonLocked());
+            System.out.println(((UserDetails)principal).isCredentialsNonExpired());
+            System.out.println(((UserDetails)principal).isEnabled());
+        } else {
+            String username = principal.toString();
+        }
         return "/index";
     }
     @RequestMapping(value = {"/logined"})
